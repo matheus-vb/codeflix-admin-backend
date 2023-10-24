9704f7d989743e1d26c066922aa187f9ba896985
Matheus Batista <matheus.vbatista04@gmail.com>
2023-10-24 13:46:01 -0300

    feature(category-use-cases): add tests for listing categories

--- application/src/test/java/com/matheusvb/admin/catalogue/application/category/retrieve/GetCategoryByIdUseCaseTest.java
+++ application/src/test/java/com/matheusvb/admin/catalogue/application/category/retrieve/GetCategoryByIdUseCaseTest.java
@@ -4,6 +4,7 @@ import com.matheusvb.admin.catalogue.application.category.create.DefaultCreateCa
 import com.matheusvb.admin.catalogue.domain.category.Category;
 import com.matheusvb.admin.catalogue.domain.category.CategoryGateway;
 import com.matheusvb.admin.catalogue.domain.category.CategoryID;
+import com.matheusvb.admin.catalogue.domain.exceptions.DomainException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -41,7 +42,9 @@ public class GetCategoryByIdUseCaseTest {
         final var expectedDescription = "A movie category";
         final var expectedIsActive = true;
 
-        final var category = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var expectedCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var category = Category.newCategory(expectedCategory.getName(), expectedCategory.getDescription(), expectedCategory.isActive());
         final var expectedId = category.getId();
 
         when(categoryGateway.findById(eq(expectedId)))
@@ -49,8 +52,7 @@ public class GetCategoryByIdUseCaseTest {
 
         final var actualOutput = useCase.execute(expectedId.getValue());
 
-        Assertions.assertNotNull(actualOutput);
-        Assertions.assertNotNull(actualOutput.id());
+        Assertions.assertEquals(CategoryOutput.from(expectedCategory), actualOutput);
 
         Mockito.verify(categoryGateway, Mockito.times(1))
                 .findById(Mockito.argThat(foundCategory -> {
@@ -67,13 +69,33 @@ public class GetCategoryByIdUseCaseTest {
 
     @Test
     public void givenAnInvalidId_whenCallGetCategoryById_thenShouldReturnNotFound() {
+        final var expectedErrorMessage = "";
+        final var expectedId = CategoryID.from("123");
 
+        when(categoryGateway.findById(expectedId))
+            .thenReturn(Optional.empty());
 
+        final var actualException = Assertions.assertThrows(
+                DomainException.class,
+                () -> useCase.exectute(expectedId.getValue())
+        );
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
     }
 
     @Test
     public void givenAValidId_whenGatewayThrowsException_thenShouldReturnException() {
+        final var expectedErrorMessage = "Gateway error";
+        final var expectedId = CategoryID.from("123");
 
+        when(categoryGateway.findById(eq(expectedId)))
+                .thenThrow(new IllegalStateException(expectedErrorMessage));
 
+        final var actualException = Assertions.assertThrows(
+                IllegalStateException.class,
+                () -> useCase.execute(expectedId.getValue())
+        );
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
     }
 }
