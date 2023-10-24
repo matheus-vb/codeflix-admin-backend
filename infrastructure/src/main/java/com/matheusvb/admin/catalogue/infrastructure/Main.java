b7461ea446ed31dc20faef4d1fc1e052671e21bb
Matheus Batista <matheus.vbatista04@gmail.com>
2023-10-24 17:23:52 -0300

    chore(springboot): add springboot dependency

--- infrastructure/src/main/java/com/matheusvb/admin/catalogue/infrastructure/Main.java
+++ infrastructure/src/main/java/com/matheusvb/admin/catalogue/infrastructure/Main.java
@@ -1,7 +1,10 @@
 package com.matheusvb.admin.catalogue.infrastructure;
 
 import com.matheusvb.admin.catalogue.application.UseCase;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello application");
