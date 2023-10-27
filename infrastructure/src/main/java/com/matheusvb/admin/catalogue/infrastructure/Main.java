51022a7055a2616029533403bb0adb4242ea8285
Matheus Batista <matheus.vbatista04@gmail.com>
2023-10-27 00:24:35 -0300

    chore(springboot): create development profile

--- infrastructure/src/main/java/com/matheusvb/admin/catalogue/infrastructure/Main.java
+++ infrastructure/src/main/java/com/matheusvb/admin/catalogue/infrastructure/Main.java
@@ -4,11 +4,14 @@ import com.matheusvb.admin.catalogue.application.UseCase;
 import com.matheusvb.admin.catalogue.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello application");
+
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
 
         SpringApplication.run(WebServerConfig.class, args);
     }
