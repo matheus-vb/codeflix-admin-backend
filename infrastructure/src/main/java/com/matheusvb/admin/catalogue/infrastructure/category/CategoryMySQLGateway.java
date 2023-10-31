6d908cdc9c3b0e559efd9f1e5dcf136944174685
Matheus Batista <matheus.vbatista04@gmail.com>
2023-10-31 13:11:29 -0300

    feature(category-jpa): implement create category

--- infrastructure/src/main/java/com/matheusvb/admin/catalogue/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/com/matheusvb/admin/catalogue/infrastructure/category/CategoryMySQLGateway.java
@@ -5,6 +5,7 @@ import com.matheusvb.admin.catalogue.domain.category.CategoryGateway;
 import com.matheusvb.admin.catalogue.domain.category.CategoryID;
 import com.matheusvb.admin.catalogue.domain.category.CategorySearchQuery;
 import com.matheusvb.admin.catalogue.domain.pagination.Pagination;
+import com.matheusvb.admin.catalogue.infrastructure.category.persistence.CategoryJPAEntity;
 import com.matheusvb.admin.catalogue.infrastructure.category.persistence.CategoryRepository;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +21,9 @@ public class CategoryMySQLGateway implements CategoryGateway {
     }
 
     @Override
-    public Category create(Category aCategory) {
-        return null;
+    public Category create(final Category aCategory) {
+        return this.repository.save(CategoryJPAEntity.from(aCategory))
+                .toAggregate();
     }
 
     @Override
